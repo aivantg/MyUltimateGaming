72001dd76c9e29d5a4590599cc9b43fd4751ebde
aivantg <aivant.developer@gmail.com>
2020-03-29 16:13:53 -0500

    initial commit

---
+++ index.php
@@ -0,0 +1,202 @@
+
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<head>
+
+<title>My Ultimate Gaming, poker rooms, sportsbook, slots, table games and online casino's.</title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+
+<meta name="keywords" content="amazon, discount prices, cheap, bookstore, music, dvd, electronics, kids, baby, home, garden, gifts, registries, cheap hotel, discount hotel, art, digital cameras, clothing, shoes, collectibles, computers, electronics, dolls, bears, movies, memorabilia, jewelry, watches, musical instruments, glass, sporting goods, sports cards, stamps, tickets, toys, hobbies, online casinos, sportsbook, poker room, odds maker casino.">
+<meta name="description" content="We make it easy to locate low priced, discount and clearance books, music, electronics, DVDs and movies, discount hotel rates, using our links to the most popular stores and online casino's.">
+
+<link href="assets/mystyle.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+
+<div align="center">
+<table width="754" cellpadding="0" cellspacing="0" border="0">
+<tbody>
+<tr>
+<td valign="top">
+<table width="754" cellpadding="0" cellspacing="0" border="0">
+<tbody>
+<tr>
+<td height="60" width="150" valign="middle" align="center" bgcolor="#FFFFFF" style="background-image:url('assets/flagbg.jpg')"><img src="assets/flag9.gif" width="105"
+height="70"></td>
+<td height="70" bgcolor="#FFFFFF" background="assets/tbg1.gif"
+width="604">
+<table style="filter:GLOW(color=#C4000," strength="4)" width="604">
+<tbody>
+<tr>
+<td>&#160;&#160;&#160;<font size="6" color="#FFFFFF"><b>My Ultimate Gaming</b></font></td>
+</tr>
+</tbody>
+</table>
+</td>
+</tr>
+
+<tr>
+<td background="assets/basebg2.jpg" colspan="2" height="24">
+<div id="navbar2" align="center">
+<ul>
+<li><a href="index.php">HOME</a></li>
+
+<li><a href="affiliates.php">AFFILIATES</a></li>
+</ul></div>
+</td>
+</tr>
+</tbody>
+</table>
+</td>
+</tr>
+
+<tr>
+<td valign="top">
+<table width="100%" cellpadding="0" cellspacing="0" background=
+"assets/topnavbg.jpg" border="0">
+<tbody>
+<tr>
+<td width="150" rowspan="2" valign="top">
+<table width="100%" border="0" cellspacing="0" cellpadding="0">
+<tbody>
+<tr>
+<td valign="top"><img src="assets/main.gif" width="150" height=
+"392"><br />
+<object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=5,0,0,0" width=150 height=350><param name=movie value="cashfinal.swf"> <param name=quality value=best> <param name=wmode VALUE=> <PARAM NAME=bgcolor value=#000000> <embed src="cashfinal.swf" quality=best bgcolor=#000000 width=150 height=350 type="application/x-shockwave-flash" pluginspage="http://www.macromedia.com/shockwave/download/index.cgi?P1_Prod_Version=ShockwaveFlash"></embed>
+</object>
+</td>
+</tr>
+
+
+</tbody>
+</table>
+</td>
+<td valign="top" bgcolor="#FFFFFF">
+<table width="100%" border="0" cellpadding="0" cellspacing="0">
+<tbody>
+<tr>
+<td height="29" colspan="3" background="assets/navbasebg.jpg"><img
+src="assets/navbasebg.jpg" width="1" height="29"></td>
+</tr>
+
+<tr>
+<td width="40">&#160;</td>
+<td>
+<h1><br>Welcome!</h1>
+
+<p>My Ultimate Gaming searched the net and found the most popular online casinos 
+with the highest payouts. Come and enjoy all your favorite casino games from the 
+comfort of your home.<br>
+<br>
+</p>
+<table width="100%" border="0" cellspacing="0" cellpadding="5">
+<tbody>
+<tr>
+<td width="145" align="center" valign="top">
+&nbsp;</td>
+<td valign="top">
+&nbsp;</td>
+</tr>
+
+<tr>
+<td width="145" align="center" valign="top">
+<!--webbot bot="HTMLMarkup" startspan --><a href="http://www.betus.com/ats/14580/mid/1442/poker"><img src="http://www.sportsimagestore.com/showbanner.asp?affid=14580&banid=1442" border="0" alt="poker"></a><!--webbot bot="HTMLMarkup" endspan --></td>
+<td>
+<p class="producthdr">BETUS.COM</p>
+<p class="MsoNormal">At the BetUS.com Casino, what happens in Vegas, happens at 
+home! With 35 of the world&#8217;s favorite online casino games and blackjack 
+tournaments available at our casino tables, you won&#8217;t ever need to leave home 
+again BetUS.com Casino offers the best bonuses and casino promotions in the 
+business. A massive $500 free for new casino players, our 25% referral bonus, 
+and countless re-deposit offers.</td>
+</tr>
+
+<tr>
+<td width="145" align="center" valign="top">
+
+&nbsp;</td>
+<td valign="top">
+&nbsp;</td>
+</tr>
+
+<tr>
+<td width="145" align="center" valign="top">
+<!-- Affiliate Code Do NOT Modify--><a href="http://affiliates.commissionaccount.com/processing/clickthrgh.asp?btag=a_7820b_126"  target="_blank"><img src="http://affiliates.commissionaccount.com/processing/impressions.asp?btag=a_7820b_126" alt="Sportsbook Poker: The home for US Poker Players" border=0 width="125"  height="125" ></a><!-- End affiliate Code--></td>
+<td>
+<p class="producthdr">SPORTSBOOK.COM
+<p>
+Sportsbook.com is the industries leader for online sports betting. They offer 
+the best odds on all your favorite sporting events including, Football, 
+Basketball, Baseball and Hockey, just to name a few. You can even bet on the 
+ponies. As well as a great casino and multi-player poker rooms with live betting 
+lines. Free cash just for signing up. What are you waiting for? Start betting 
+now!!!</td>
+</tr>
+
+<tr>
+<td width="145" align="center">&nbsp;</td>
+<td>&nbsp;</td>
+</tr>
+<tr>
+  <td align="center" valign="top"><!--webbot bot="HTMLMarkup"
+  startspan -->
+     <a target="_blank" href="http://www.oddsmaker.com/refer.php?ID=1224a"> <img src="http://www.referincome.com/graphics/banners/om/om_casino_125x125_3.gif" border="0" /></a>
+    <!--webbot
+  bot="HTMLMarkup" endspan -->
+      <br>
+    <br></td>
+  <td valign="top"><p class="producthdr">ODDSMAKER.COM</p>
+    <p>OddsMaker.com is one of the most  reputable gaming sites online today. Offering you everything from Sportsbook  wagering, poker rooms, casino games, fantasy games, all to better accommodate  you. OddsMaker.com is the world leader in bringing you the finest quality of  entertainment to your screen, with the largest selection of online games  available in full 3D graphics and digital sound. </p>
+    <p class="producthdr">&nbsp;</p>
+    <p></td>
+</tr>
+
+<tr>
+<td width="145" align="center">&#160;</td>
+<td>&#160;</td>
+</tr>
+</tbody>
+</table>
+
+<p>&#160;</p>
+</td>
+<td width="25">&#160;</td>
+</tr>
+</tbody>
+</table>
+</td>
+</tr>
+
+<tr>
+<td bgcolor="#FFFFFF">&#160;</td>
+</tr>
+</tbody>
+</table>
+</td>
+</tr>
+
+<tr>
+<td height="29" background="assets/navbasebg2.jpg"><img src=
+"assets/basebg1.jpg" width="1" height="29"></td>
+</tr>
+
+<tr>
+<td height="44" valign="top" background="assets/basebg2.jpg">
+<table width="100%" border="0" cellspacing="0" cellpadding="0">
+<tbody>
+<tr>
+<td class="baseline"><br>
+ &#160;&#160;&#169; Copyright 2008. Web Club, 
+All Rights Reserved.</td>
+</tr>
+</tbody>
+</table>
+</td>
+</tr>
+</tbody>
+</table>
+</div>
+</body>
+</html>
+
